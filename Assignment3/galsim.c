1f31b567103901f36a278fc76cb48310af74df4f
Emanuel Bodin <embo0739@cronstedt.it.uu.se>
2020-02-13 17:08:54 +0100

    added on function

--- Assignment3/galsim.c
+++ Assignment3/galsim.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "graphics.h"
+//#include "graphics.h"
 #include <math.h>
 
 typedef struct _particle
@@ -24,9 +24,29 @@ void keep_within_box(double* xA, double* yA) {
     *yA = 0;
 }
 
-// ./galsim 2 input_data/circles_N_2.gal 100 -5 1
-// ./compare_gal_files 3000 ../result.gal ../ref_output_data/ellipse_N_03000_after100steps.gal
+particle * read_particle(int N, FILE *fp1) {
+  unsigned char buffer[8];
+  double arr[6];
+  particle *array = malloc(N * sizeof(particle));
+  for(int i = 0; i<N; i++){
+    for(int j = 0; j<6; j++){
+        fread(buffer, sizeof(buffer), 1, fp1);
+        arr[j] = *((double*)buffer);
+    }
+    array[i].posX = arr[0];
+    array[i].posY = arr[1];
+    array[i].mass = arr[2];
+    array[i].velX = arr[3];
+    array[i].velY = arr[4];
+    array[i].b = arr[5];
+}
 
+fclose(fp1);
+return array;
+}
+
+// ./galsim 2 input_data/circles_N_2.gagraphics
+// gcc -O1 -g -pg galsim.c -lm
 int main(int argc, char* argv[]){
   if (argc != 6){
       printf("Wrong number of input arguments\n");
@@ -46,34 +66,17 @@ int main(int argc, char* argv[]){
     double delta_t = pow(10, deltaT);
 
     float L=1, W=1;
+    /*
     if (graphics) {
       InitializeGraphics(argv[0],windowWidth,windowWidth);
       SetCAxes(0,1);
-    }
+    }*/
 
     if (fp1 == NULL){
     printf("Error while opening the file.\n");
     return 1;
     }
-    
-    unsigned char buffer[8];
-    double arr[6];
-    particle *array = malloc(N * sizeof(particle));
-    for(int i = 0; i<N; i++){
-
-      for(int j = 0; j<6; j++){
-          fread(buffer, sizeof(buffer), 1, fp1);
-          arr[j] = *((double*)buffer);
-      }
-        array[i].posX = arr[0];
-        array[i].posY = arr[1];
-        array[i].mass = arr[2];
-        array[i].velX = arr[3];
-        array[i].velY = arr[4];
-        array[i].b = arr[5];
-    }
-
-    fclose(fp1);
+    particle *array = read_particle(N, fp1);
 
     particle *new_array = malloc(N * sizeof(particle));
     for (int k = 0; k < n_steps; k++) {
@@ -114,22 +117,24 @@ int main(int argc, char* argv[]){
         new_array[i].posY = piy;
         new_array[i].mass = array[i].mass;
         new_array[i].b = array[i].b;
-        
+        /*
         if (graphics) {
           keep_within_box(&array[i].posX, &array[i].posY);
           DrawCircle(array[i].posX, array[i].posY, L, W, circleRadius, circleColor);
           Refresh();
           usleep(1000);
         }
+        */
       }
       
       for(int i = 0; i<N; i++){
         array[i] = new_array[i];
       }
-      
+      /*
       if (graphics) {
         ClearScreen();
       }
+      */
     }
 
     fp2 = fopen("result.gal", "wb");
@@ -142,15 +147,16 @@ int main(int argc, char* argv[]){
       fwrite(&new_array[i].b, sizeof(double), 1, fp2);
     }
 
+
     fclose(fp2);
     free(array);
     free(new_array);
-
+  /*
   if (graphics) {
     FlushDisplay();
     CloseDisplay();
   }
-  
+  */
 
   return 0;
 }
